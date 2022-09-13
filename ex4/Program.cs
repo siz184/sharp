ed2895b18b28c4d10237208caec1e703f3252941
Andrey <siz184@ya.ru>
2022-09-13 19:55:56 +0300

    add if

---
+++ ex4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "Маша"){
+    Console.WriteLine("Ура, это Маша!");
+}else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
