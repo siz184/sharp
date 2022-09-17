982c5ada0ab1809445475d14e02330fe108d8144
Andrey <siz184@ya.ru>
2022-09-17 18:05:44 +0300

    hometask ex4

---
+++ hometask/less1/ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if(num2 > max){
+    max = num2;
+}
+if(num3 > max){
+    max = num3;
+}
+Console.WriteLine("Большее число: "+max);
