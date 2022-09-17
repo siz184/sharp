49dbbfff4e01aad43ea16a5c57420974c6f7b877
Andrey <siz184@ya.ru>
2022-09-17 18:12:45 +0300

    hometask ex6

---
+++ hometask/less1/ex6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num%2==0){
+    Console.WriteLine(num + " - Чётное число");
+}else{
+    Console.WriteLine(num + " - Нечётное число");
+}
+
