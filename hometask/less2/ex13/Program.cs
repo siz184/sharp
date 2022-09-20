52c6c558046f8c07ba003bc474244eefafbada3d
Andrey <siz184@ya.ru>
2022-09-20 21:12:49 +0300

    hometask ex15

--- hometask/less2/ex13/Program.cs
+++ hometask/less2/ex13/Program.cs
@@ -1,13 +1,12 @@
 ﻿Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int res;
 if(num<100){
     Console.WriteLine("Третьей цифры нет!");
 }else{  
     while(num > 1000){
         num = num/10;
     }
-    res = num%100%10; 
+    int res = num%100%10; 
     Console.WriteLine("Третья цифра: "+res);
 }
 
---
+++ hometask/less2/ex15/Program.cs
@@ -0,0 +1,16 @@
+﻿void weekDay(){
+    Console.Write("Введите цифру от 1 до 7: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if(num < 1 || num > 7){
+        Console.WriteLine("Цифра не в интервале от 1 до 7!");
+        weekDay();
+    }else{  
+        string res = "будни";
+        if(num==6 || num==7){
+            res = "выходной"; 
+        }
+        Console.WriteLine("День недели под номером "+num+" - "+res);
+    }
+}
+
+weekDay();
