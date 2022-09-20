396ef6e1a9eeff085c41b10f2c5298ce27ca16df
Andrey <siz184@ya.ru>
2022-09-20 19:58:55 +0300

    hometask ex10

---
+++ hometask/less2/ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿void SecondNum(){
+    Console.Write("Введите трёхзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int prov100 = num/100;
+    if(prov100<1 || prov100>10){
+        Console.WriteLine("Введено не трёхзначное число!");
+        SecondNum();
+    }else{
+        int res = num%100/10;
+        Console.Write("Вторая цифра введенного числа: "+res);
+    }
+}
+
+SecondNum();
