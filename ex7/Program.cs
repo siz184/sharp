f82b3052f33cf1b0612b7ae9189685840b11b8c3
Andrey <siz184@ya.ru>
2022-09-26 20:44:17 +0300

    ex7

---
+++ ex7/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for(int i=1;i<=num;i++){
+    result = result + i;
+}
+Console.WriteLine(result);
