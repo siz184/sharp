0a2aaf769d2b92cc32943f939fe86c5129700edf
Andrey <siz184@ya.ru>
2022-10-06 01:04:49 +0300

    hometask less6

---
+++ hometask/less6/ex41/Program.cs
@@ -0,0 +1,31 @@
+﻿string showSklonenie(int number, string[] sklonenie){
+    int lastDigit = number%10;
+    if(lastDigit == 1){
+        return $"{number} {sklonenie[0]}";
+    }else if(lastDigit>1 && lastDigit<5){
+        return $"{number} {sklonenie[1]}";
+    }else{
+        return $"{number} {sklonenie[2]}";
+    }
+}
+
+int ReadNums(){
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if(num > 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int M = 4;
+int count = 0;
+string[] arrSkl = {"число","числа","чисел"};
+
+while(M>0){
+    count += ReadNums();
+    M--;
+}
+
+Console.WriteLine($"Введено {showSklonenie(count,arrSkl)} больше нуля!");
---
+++ hometask/less6/ex43/Program.cs
@@ -0,0 +1,23 @@
+﻿double ReadNums(string text){
+    Console.Write($"Введите {text}: ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double b1 = ReadNums("b1");
+double k1 = ReadNums("k1");
+double b2 = ReadNums("b2");
+double k2 = ReadNums("k2");
+
+
+if (k1 == k2 && b1 == b2){
+    Console.WriteLine("Прямые совпадают");
+}else if (k1 == k2){
+    Console.WriteLine("Прямые параллельны");
+}else{
+
+    double x = (b1-b2)/(k2-k1);
+    double y = (k2*b1-k1*b2)/(k2-k1);
+
+    Console.WriteLine($"x:{x}, y:{y}");
+
+}
